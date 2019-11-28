441811b2eee3d4375fb5c82e08d79ad829e1e8c8
Shirone0110 <lam.le@centre.edu>
2019-11-28 00:09:48 -0500

    finish part 2

--- main.js
+++ main.js
@@ -9,20 +9,34 @@ d3.csv("Data/Wagegap.csv", Wagegap).then(function(data)
     setup(data);
 })
 
-var screen = {width: 1000, height: 600};
-var margins = {top: 50, right: 50, bottom: 50, left: 50};
+var setIndustry = function(d)
+{
+    return {
+        Industry: d.Industry,
+        WomenEarning: parseFloat(d.Womensearnings),
+        MenEarning: parseFloat(d.Mensearnings),
+        PercentEarning: parseFloat(d.Womensearnings) / parseFloat(d.Mensearnings) * 100,
+        NoWomen: parseFloat(d.Numberofwomen),
+        NoMen: parseFloat(d.Numberofmen),
+        PercentNo: parseFloat(d.Numberofwomen) / parseFloat(d.Numberofmen) * 100
+    }
+}
 
 var setup = function(dataset)
 {
+    var screen = {width: 1000, height: 600};
+    var margins = {top: 50, right: 50, bottom: 50, left: 50};
+
+    var width = screen.width - margins.left - margins.right;
+    var height = screen.height - margins.top - margins.bottom;
+    
     d3.select("svg")
         .attr("width", screen.width)
         .attr("height", screen.height)
         .append("g")
         .attr("id", "graph1")
         .attr("transform", "translate(" + margins.left + "," + margins.top + ")")
-    
-    var width = screen.width - margins.left - margins.right;
-    var height = screen.height - margins.top - margins.bottom;
+        .classed("hidden", false);
     
     var xScale = d3.scaleLinear()
                     .domain([0, 29])
@@ -34,26 +48,25 @@ var setup = function(dataset)
     
     var xAxis = d3.axisBottom(xScale);
     var yAxis = d3.axisLeft(yScale);
-    d3.select("svg")
+    d3.select("#graph1")
         .append("g")
         .classed("axis", "true");
     
     d3.select(".axis")
         .append("g")
         .attr("id", "xAxis")
-        .attr("transform", "translate(" + margins.left + "," + (margins.top + height) + ")")
+        .attr("transform", "translate(0," + height + ")")
         .call(xAxis);
     
     d3.select(".axis")
         .append("g")
         .attr("id", "yAxis")
-        .attr("transform", "translate(" + margins.left + "," + margins.top + ")")
         .call(yAxis);
     
-    draw(dataset, xScale, yScale);
+    draw1(dataset, xScale, yScale);
 }
 
-var draw = function(dataset, xScale, yScale)
+var draw1 = function(dataset, xScale, yScale)
 {
     //console.log(dataset);
     d3.select("#graph1")
@@ -73,7 +86,7 @@ var draw = function(dataset, xScale, yScale)
         .attr("r", 3)
         .attr("id", function(num, index)
         {
-            return index;
+            return "no" + index;
         })
     
     var line = d3.select("#graph1")
@@ -91,4 +104,134 @@ var draw = function(dataset, xScale, yScale)
     line.append("path")
         .datum(dataset)
         .attr("d", lineGenerator);
+    
+    d3.select("#no27")
+        .on("mouseover", function()
+        {
+            var label = "Click me!";
+            d3.select("#tooltip")
+                .text(label)
+                .style("left", (d3.event.pageX + 20) + "px")
+                .style("top", (d3.event.pageY - 28) + "px")
+                .classed("hidden", false);
+        })
+        .on("mouseout", function()
+        {
+            d3.select("#tooltip")
+                .classed("hidden", true);
+        })
+}
+
+d3.select("svg")
+    .on("click", function()
+    {
+        d3.csv("Data/Industry.csv", setIndustry).then(function(data)
+        {
+            console.log(data);
+            setup2(data);
+        })
+    })
+
+var setup2 = function(dataset)
+{
+    var screen = {width: 1000, height: 800};
+    var margins = {top: 50, right: 50, bottom: 50, left: 300};
+
+    var width = screen.width - margins.left - margins.right;
+    var height = screen.height - margins.top - margins.bottom;
+    
+    d3.select("svg")
+        .attr("width", screen.width)
+        .attr("height", screen.height)
+        .append("g")
+        .attr("id", "graph2")
+        .attr("transform", "translate(" + margins.left + "," + margins.top + ")")
+        .classed("hidden", false);
+    
+    d3.select("#graph1")
+        .classed("hidden", true);
+    
+    var yScale = d3.scaleBand()
+        .domain(dataset.map(function(d){return d.Industry;}))
+        .range([0, height])
+    
+    var lowxScale = d3.scaleLinear()
+        .domain([0, 110])
+        .range([0, width])
+    
+    var NumyScale = d3.scaleLinear()
+        .domain([0, 14])
+        .range([5, height - 45])
+    
+    var highxScale = d3.scaleLinear()
+        .domain([0, 280])
+        .range([0, width])
+    
+    var lowxAxis = d3.axisBottom(lowxScale);
+    var yAxis = d3.axisLeft(yScale);
+    var highxAxis = d3.axisTop(highxScale);
+    
+    d3.select("#graph2")
+        .append("g")
+        .classed("graph2axis", "true");
+    
+    d3.select(".graph2axis")
+        .append("g")
+        .attr("id", "lowxAxis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(lowxAxis)
+        .attr("font-size", 14);
+    
+    d3.select(".graph2axis")
+        .append("g")
+        .attr("id", "graph2yAxis")
+        .call(yAxis)
+        .attr("font-size", 14);
+    
+    d3.select(".graph2axis")
+        .append("g")
+        .attr("id", "highxAxis")
+        .call(highxAxis)
+        .attr("font-size", 14);
+    
+    draw2(dataset, lowxScale, highxScale, NumyScale);
+}
+
+var draw2 = function(dataset, lowxScale, highxScale, yScale)
+{   
+    d3.select("#graph2")
+        .append("g")
+        .attr("id", "PercentEarning")
+        .selectAll("rect")
+        .data(dataset)
+        .enter()
+        .append("rect")
+        .attr("y", function(d, i)
+        {
+            return yScale(i) + 20;
+        })
+        .attr("height", 20)
+        .attr("width", function(d)
+        {
+            return lowxScale(d.PercentEarning);
+        })
+        .attr("fill", "red")
+    
+    d3.select("#graph2")
+        .append("g")
+        .attr("id", "PercentNo")
+        .selectAll("rect")
+        .data(dataset)
+        .enter()
+        .append("rect")
+        .attr("y", function(d, i)
+        {
+            return yScale(i);
+        })
+        .attr("height", 20)
+        .attr("width", function(d)
+        {
+            return highxScale(d.PercentNo);
+        })
+        .attr("fill", "blue")
 }
